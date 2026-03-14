6c14c723a81142ed92f787a742725c3e626a71c8
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-03-14 12:40:40 +0000

    Updated app.js with decrement properties

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -7,15 +7,24 @@ function increment(node) {
   node.textContent = Number(current) + 1;
 }
 
+function decrement(node) {
+  let current = node.textContent;
+  node.textContent = Number(current) - 1;
+}
+
 export function App() {
   const body = document.createElement("body");
   main(body);
   header(body);
 
   const button = body.querySelector("#increment");
+  const buttonDecrement = body.querySelector("#decrement");
   const counter = body.querySelector("#counter");
   button.addEventListener("click", () => {
     increment(counter);
+  });
+  buttonDecrement.addEventListener("click", () => {
+    decrement(counter);
   });
 
   return body;
