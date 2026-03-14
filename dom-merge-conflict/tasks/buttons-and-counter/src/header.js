0b59896c15cd067ab1af32a992970631b51007d7
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-03-14 12:47:16 +0000

    Fixed errors with test

--- dom-merge-conflict/tasks/buttons-and-counter/src/header.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/header.js
@@ -2,7 +2,7 @@ export function header(body) {
   const header = document.createElement("header");
   header.innerHTML = `
             <h1>Number Counter</h1>
-            <p>A simple counter. Press increment to increase the count by one.</p>
+            <p>A simple counter. Press increment to increase the count by one and decrement to decrease the count by 1.</p>
         `;
   body.appendChild(header);
 }
